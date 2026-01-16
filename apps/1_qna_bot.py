89992983446be8e204e33bb37bcc560822bac959
AkanshuMittal <akanshumittal150@gmail.com>
2026-01-16 16:13:44 +0530

    Update

--- apps/1_qna_bot.py
+++ apps/1_qna_bot.py
@@ -1,19 +1,28 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-
+import streamlit as st
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-while True:
-    query = input("User: ")
+st.title("AskBuddy - AI Qna Bot")
+st.markdown("My Qna Bot with Langchain and Google Gemini !")
 
-    if query.lower() in ["quit","bye","exit"]:
-        print("GoodBye")
-        break
-
+query = st.chat_input("Ask Anyhting?")
+if query:
+    st.chat_message("user").markdown(query)
     result = llm.invoke(query)
+    st.chat_message("ai").markdown(result.content)
 
-    print("AI: ", result.content, "\n")
+# while True:
+#     query = input("User: ")
+
+#     if query.lower() in ["quit","bye","exit"]:
+#         print("GoodBye")
+#         break
+
+#     result = llm.invoke(query)
+
+#     print("AI: ", result.content, "\n")
 
 
