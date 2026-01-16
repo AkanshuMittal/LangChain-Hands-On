2277a450ca3fa47231dde1b520b989a6309e94a8
AkanshuMittal <akanshumittal150@gmail.com>
2026-01-16 16:42:09 +0530

    Update file

--- apps/1_qna_bot.py
+++ apps/1_qna_bot.py
@@ -8,11 +8,21 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 st.title("AskBuddy - AI Qna Bot")
 st.markdown("My Qna Bot with Langchain and Google Gemini !")
 
+if "messages" not in st.session_state:
+      st.session_state.messages = []
+
+for message in st.session_state.messages:
+    role = message["role"]
+    content=message["content"]
+    st.chat_message(role).markdown(content)
+    
 query = st.chat_input("Ask Anyhting?")
 if query:
+    st.session_state.messages.append({"role":"user", "content": query})
     st.chat_message("user").markdown(query)
     result = llm.invoke(query)
     st.chat_message("ai").markdown(result.content)
+    st.session_state.messages.append({"role": "ai", "content": result.content})
 
 # while True:
 #     query = input("User: ")
